9f7ef3a315da84fcd65a63192b8d2318d345072e
Ubuntu <ubuntu@ip-172-31-19-89.us-west-2.compute.internal>
2014-07-08 04:03:14 +0000

    Completed assignment1, intro to node.js

---
+++ homework_assingments/assignment1/README.md
@@ -0,0 +1,19 @@
+Intoduction to node.js
+======================
+
+Prerequisites
+- Filesystem basics
+- SSH
+- SCP
+- Installing node and npm
+- Editing with command line
+- A second program: using the libraries
+- A third node program: Fibonacci numbers
+
+Part 1
+------------
+In part 1, write a node.js script that writes the output "A startup is a business built to grow rapidly.\n" to a file.
+
+Part 2
+----------
+write a node.js script that writes the first 100 prime numbers to a file, with each prime separated by a comma.
---
+++ homework_assingments/assignment1/prime.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+// isPrime 
+// return true if n is a prime number, else return false 
+var isPrime = function(n) {
+  var i;
+  var isPrime = true;
+  for(i = 2; i < n; i++) {
+    if(n % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  return isPrime
+};
+
+
+
+var firstkprime = function(k) {
+  var i; 
+  var arr = [];
+
+  for (i = 2; i < k+1; i++) { // 1 is not a prime, so start at 2
+    if (isPrime(i)) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+};
+
+// Print to console
+var fmt = function(arr) {
+  return arr.join(" ");
+};
+
+var k = 100;
+console.log("firstkprime(" + k + ")");
+console.log(fmt(firstkprime(k)));
